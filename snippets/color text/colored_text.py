27e91b6769113eb26251104dd6abb387958a5afa
Tabornoti <dapikg00@gmail.com>
2022-05-21 16:10:29 +0200

    Create colored_text.py
    colored text in python

---
+++ snippets/color text/colored_text.py
@@ -0,0 +1,19 @@
+
+square = '\033[0;30;47m'
+dossc =  '\033[0;37;44m'
+red = '\033[1;31;40m'
+green = '\033[1;32;40m'
+blue = '\033[1;34;40m'
+
+# square is a text on a gray background
+# doss is a white text on MS-DOS like blue background
+# red is a red text on black background
+# green is a green text on black background
+# blue is a blue text on black background
+
+# example
+print(green, 'My cool green text on black background')
+
+# print(<color>, 'text to be colored')
+
+# Author: Tabornoti
